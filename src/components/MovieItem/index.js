9e45751d61c68493dac97b5604c813f7257b5558
avik067 <avikbiswas067@gmail.com>
2023-09-06 18:40:48 +0000

    popup-content

--- src/components/MovieItem/index.js
+++ src/components/MovieItem/index.js
@@ -1,12 +1,48 @@
+import Popup from 'reactjs-popup'
+
+import ReactPlayer from 'react-player'
+
+import 'reactjs-popup/dist/index.css'
+
+import {IoMdClose} from 'react-icons/io'
+
 import './index.css'
 
 const MovieItem = ({details}) => {
   const {videoUrl, thumbnailUrl} = details
 
   return (
-    <div>
-      <img className="each-card" src={thumbnailUrl} alt="" />
-    </div>
+    <Popup
+      modal="true"
+      trigger={
+        <button className="trigger-button" type="button">
+          <img src={thumbnailUrl} alt="" />
+        </button>
+      }
+    >
+      {close => (
+        <>
+          <div className="close-container">
+            <div>
+              <button
+                type="button"
+                className="trigger-button"
+                onClick={() => close()}
+              >
+                <IoMdClose className="icon-close" />
+              </button>
+            </div>
+          </div>
+
+          <ReactPlayer
+            className="player-container"
+            playing="true"
+            controls="true"
+            url={videoUrl}
+          />
+        </>
+      )}
+    </Popup>
   )
 }
 
